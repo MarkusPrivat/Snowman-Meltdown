2e30dbe622d1c86359f21dff35a981a98f40f791
MarkusPrivat <markus.privat@gmail.com>
2026-02-09 22:10:52 +0100

    moved the ascii art to a separate file

--- snowman.py
+++ snowman.py
@@ -1,36 +1,10 @@
 import random
 
+import ascii_art as art
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
-# Snowman ASCII Art stages
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
+
 
 def get_random_word():
     """
@@ -49,7 +23,7 @@ def display_game_state(mistakes: int, secret_word: str, guessed_letters: list):
     :return: 
     """
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
-    print(STAGES[mistakes])
+    print(art.STAGES[mistakes])
     print(f"Word: {"_" * len(secret_word)}")
     print(f"You guessed: {guessed_letters}")
 
