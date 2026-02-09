b04292849d6c0b49a42a83c62f2019588abe7a44
MarkusPrivat <markus.privat@gmail.com>
2026-02-10 00:15:03 +0100

    Added a replay Option: Ask the user if they want to play again after a game ends.

--- snowman.py
+++ snowman.py
@@ -1,12 +1,32 @@
 import game_logic as gl
 
 
+def new_round() -> bool:
+    """
+    Ask the user if they want to play again after a game ends.
+    :return: new round
+    """
+    while True:
+        user_input = input("Do you want to play now? (yes/no): ")
+        try:
+            if not user_input.lower() in ['yes', 'y', 'no', 'n']:
+                raise ValueError("Invalid input: must be 'yes' or 'no'.")
+            if user_input.lower() in ["yes", "y"]:
+                return True
+            return False
+        except ValueError as error:
+            print(error)
+
+
 def main():
     """
     The main function calls play_game()
     :return: None
     """
-    gl.play_game()
+    while True:
+        gl.play_game()
+        if not new_round():
+            break
 
 
 if __name__ == "__main__":
